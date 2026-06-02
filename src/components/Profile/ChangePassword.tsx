3528de5514f9ecc85f6cf5ed19a4df35a4cd4d3e
YC <ug000sx4@gmail.com>
2026-06-02 09:17:33 +0200

    Add ChangePassword and EditProfile components

--- src/components/Profile/ChangePassword.tsx
+++ src/components/Profile/ChangePassword.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const ChangePassword = () => {
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    const handleClickClear = () => {
+        setCurrentPassword("");
+        setNewPassword("");
+        setConfirmNewPassword("");
+    }
+
     return (
         <div>
-            <h1>Change Password</h1>
+            <label>
+                Current Password:
+                <input type="password"
+                    value={currentPassword}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+            </label>
+            <label>
+                New Password:
+                <input type="password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                />
+            </label>
+            <label>
+                Confirm New Password:
+                <input type="password"
+                    value={confirmNewPassword}
+                    onChange={(e) => setConfirmNewPassword(e.target.value)}
+                />
+            </label>
+            {/* TODO: Add logic to save and close */}
+            <button>Save and close</button>
+            {/* TODO: Add logic to close without saving */}
+            <button>Close without saving</button>
+            <button onClick={handleClickClear}>Clear</button>
         </div>
     );
 };
--- src/components/Profile/EditProfile.tsx
+++ src/components/Profile/EditProfile.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const EditProfile = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const handleClickClear = () => {
+        setFirstName("");
+        setLastName("");
+    }
+
     return (
         <div>
-            <h1>Edit Profile</h1>
+            <label>
+                First Name:
+                <input type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </label>
+            <label>
+                Last Name:
+                <input type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </label>
+            {/* TODO: Add logic to save and close */}
+            <button >Save and close</button>
+            {/* TODO: Add logic to close without saving */}
+            <button>Close without saving</button>
+            <button onClick={handleClickClear}>Clear</button>
         </div>
     );
 };
