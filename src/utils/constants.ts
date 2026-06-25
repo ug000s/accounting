0795c0336299ce49b30bf620b085bb90fef054c9
YC <ug000sx4@gmail.com>
2026-06-25 14:03:29 +0200

    Update BASE_URL in constants to use http://localhost:8080 for local development

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ export const UPDATE_MODE_DEFAULT = 'UPDATE_MODE_DEFAULT';
 export const UPDATE_MODE_EDIT_PROFILE = 'UPDATE_MODE_EDIT_PROFILE';
 export const UPDATE_MODE_CHANGE_PASSWORD = 'UPDATE_MODE_CHANGE_PASSWORD';
 
-export const BASE_URL = 'https://webaccounting.herokuapp.com';
+export const BASE_URL = 'http://localhost:8080';
 
 export const createToken = (userName: string, password: string) => `Basic ${btoa(`${userName}:${password}`)}`;
